66d3ef13e40cc5884c8b2b60d70bfb82737c21ca
izzibizz <contact@izabellind.com>
2025-01-07 16:45:30 +0100

    fix mobile version

--- src/development/pages/FrontendProjects.jsx
+++ src/development/pages/FrontendProjects.jsx
@@ -21,13 +21,13 @@ export const FrontendProjects = () => {
       <img
         src="/frontend-developer-w.svg"
         className="w-[90px] tablet:w-[150px] "
-      /><ul className=" mx-auto text-white flex flex-col gap-16">
+      /><ul className=" mx-auto text-white flex flex-col gap-24">
       {devData.map((project, index) => (
-        <li key={index} className="flex gap-10">
-        <img src={project.images[0].url} alt={project.images.alt} className="w-1/3 rounded" />
+        <li key={index} className="flex flex-col tablet:flex-row gap-4 laptop:gap-10">
+        <img src={project.images[0].url} alt={project.images.alt} className="laptop:w-1/3 rounded" />
         <div className="flex flex-col gap-4">
           <h3 className="text-xl">{project.title}</h3>
-        <p className="text-sm w-11/12 text-justify">{project.description}</p>
+        <p className="text-sm laptop:w-11/12 text-justify">{project.description}</p>
         </div>
         
         </li>
