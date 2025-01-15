27e94dc58d631ca1ea27fabed291c1d7e2d59298
izzibizz <contact@izabellind.com>
2025-01-15 10:52:31 +0100

    about

--- src/development/pages/FrontendAbout.jsx
+++ src/development/pages/FrontendAbout.jsx
@@ -11,8 +11,9 @@ export const FrontendAbout = () => {
     }, [])
 
   return (
-    <section className="text-white animate-fadeIn">
-      Frontend about
+    <section className="text-white animate-fadeIn flex flex-col w-10/12 laptop:w-9/12 mx-auto mt-32">
+      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736934642/about-me-white_vitglw.svg" className="h-[100px] w-auto self-start"/>
+      <img src="https://res.cloudinary.com/dbf8xygxz/image/upload/v1736934392/IZABEL.LIND-frontend-developer-artist_dzqpyd.jpg" alt="portrait Izabel Lind" className="w-full tablet:w-[500px] rounded-xl self-end" />
     </section>
   )
 }
