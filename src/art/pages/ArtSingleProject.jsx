167dfdf2141570736c23be90dc8b816a57447fe9
izzibizz <contact@izabellind.com>
2025-01-20 22:56:18 +0100

    single art proj

--- src/art/pages/ArtSingleProject.jsx
+++ src/art/pages/ArtSingleProject.jsx
@@ -37,22 +37,30 @@ export const ArtSingleProject = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(artProjects)
+  console.log(project)
     
   return (
     <section className="animate-fadeIn font-body flex flex-col gap-10 mb-20">
            <NavLink to={`/art`}>
-          <SlArrowLeft className="cursor-hollow pl-2 w-6 h-6 laptop:w-8 laptop:h-6 absolute z-20 top-40 laptop:top-[80%] laptop:left-8 hover:scale-125" />{" "}
+          <SlArrowLeft className="cursor-hollow pl-2 w-6 h-6 laptop:w-8 laptop:h-6 absolute z-20 bottom-32 left-2 laptop:bottom-[10%] laptop:left-6 hover:scale-125" />{" "}
         </NavLink>
-      <div className="fixed bottom-10 left-4 laptop:left-10 bg-light bg-opacity-80 p-4 rounded-xl w-full laptop:w-[300px]">
+      <div className="fixed bottom-0 left-0 laptop:left-20 laptop:bottom-10 bg-light bg-opacity-80 p-4 laptop:rounded-xl w-full laptop:w-[300px]">
       <h3>{project.title}, {project.year}</h3>
-      <p className="font-light text-justify">{project.description}</p>
+      <p className="font-light">{project.description}</p>
       </div>
       {(project.images?.length === 1) && (
         <div className="w-1/2 self-end">
           <img src={project.images?.[0]?.url} alt={project.title} className="w-full"/>
         </div>
       )}
+      {/* mobile and tablet */}
+      <div className="grid laptop:hidden grid-cols-2 gap-2">
+        {project.images?.map((image, index) => (
+          <img src={image.url} key={index} className="aspect-[3/4] object-cover" alt={image.alt || project.title}/>
+        ))}
+      </div>
+      {/* laptop */}
+      <div className="flex-col gap-4 hidden laptop:flex">
       {(project.images?.length > 1) && (
       <div className={`${project.images?.length < 3 ? "tablet:w-full":"tablet:w-3/4" } w-full  flex justify-between`}>
       <img src={project.images?.[0]?.url} alt={project.title} className={`${project.images?.length < 3 ? "h-[500px]" : "h-[400px]" }`}/>
@@ -77,6 +85,7 @@ export const ArtSingleProject = () => {
       <div className="w full tablet:w-1/2 mx-auto flex justify-between">
       <img src={project.images?.[9]?.url} alt={project.title} className="h-[400px]"/>
       </div>)}
+      </div>
     </section>
   )
 }
